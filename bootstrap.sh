d91f115d5448e9f1e3d0239f98236871d5583b1e
Nathan Caldwell <saintdev@gmail.com>
2007-08-16 13:28:32 -0600

    Move autotools files to config/ subdir.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 
+rm -rf config
+mkdir -p config
 aclocal \
 && libtoolize --force --copy \
+&& aclocal \
 && autoheader \
-&& automake --add-missing --copy \
+&& automake --foreign  --add-missing --copy \
 && autoconf
