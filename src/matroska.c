d2a11e89c949a807b1b4b325e043cf9142dc89a5
Nathan Caldwell <saintdev@gmail.com>
2008-01-25 00:12:46 -0700

    Add a hack to set the first Cluster timecode to 0 if it isn't already.

--- src/matroska.c
+++ src/matroska.c
@@ -203,12 +203,22 @@ int   mk_flushFrame(mk_Writer *w, mk_Track *track) {
 
     w->cluster.pointer = w->f_pos - w->segment_ptr;
 
+	/*
+	 * Dirty HACK: This is for HandBrake/VLC. The first timecode we get isnt't
+	 * always 0, if not we set it to 0.
+	 */
+	if (w->cluster.count == 0 && w->vlc_compat && track->frame.timecode != 0) {
+		w->cluster.tc_scaled = 0;
+		delta = track->frame.timecode/w->timescale;
+	} else {
+		delta = 0;
+	}
+
     if (w->vlc_compat)
       CHECK(mk_writeSeek(w, w->cluster.seekhead, 0x1f43b675, w->cluster.pointer));
 
     CHECK(mk_writeUInt(w->cluster.context, 0xe7, w->cluster.tc_scaled)); // Cluster Timecode
 
-    delta = 0;
     w->cluster.block_count = 0;
   }
 
