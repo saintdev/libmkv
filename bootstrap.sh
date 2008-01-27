4d3ab8fb618a25646f806f9b44a9b86b190d514a
Nathan Caldwell <saintdev@gmail.com>
2008-01-27 13:08:29 -0700

    Update bootstrap.sh to just run autoreconf.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,12 @@
 
 rm -rf config
 mkdir -p config
-aclocal \
-&& libtoolize --force --copy \
-&& aclocal \
-&& autoheader \
-&& automake --foreign  --add-missing --copy \
-&& autoconf
+
+#aclocal \
+#&& libtoolize --force --copy \
+#&& aclocal \
+#&& autoheader \
+#&& automake --foreign  --add-missing --copy \
+#&& autoconf
+
+autoreconf --force --install
